1e4470e285d13a8b223c0628b21f361b23716917
Damian Mazurek <alexdrop93@gmail.com>
2018-01-23 18:12:15 +0100

    Calculator

--- src/main/java/somePackage/Calculator.java
+++ src/main/java/somePackage/Calculator.java
@@ -1,4 +1,16 @@
 package somePackage;
 
 public class Calculator {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+    public float minus(float a, float b) {
+        return a - b;
+    }
+    public float times(float a, float b) {
+        return a * b;
+    }
+    public float by(float a, float b) {
+        return a / b;
+    }
 }
